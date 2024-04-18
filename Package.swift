88e6e55ac00c7e6d2b8782d11a5088b88cc3bffc
runner <runner@Mac-1713465483819.local>
2024-04-18 18:53:56 +0000

    KMP SPM package release for 0.1.11

--- Package.swift
+++ Package.swift
@@ -1,7 +1,11 @@
 // swift-tools-version:5.3
 import PackageDescription
 
+// BEGIN KMMBRIDGE VARIABLES BLOCK (do not edit)
+let remoteKotlinUrl = "https://api.github.com/repos/touchlab-lab/ReleaseTest/releases/assets/162918047.zip"
+let remoteKotlinChecksum = "82a9ba211cb2f5ebb48717cff72e125c32ebd60d83789688b770782fac8ad7c0"
 let packageName = "allshared"
+// END KMMBRIDGE BLOCK
 
 let package = Package(
     name: packageName,
@@ -17,7 +21,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: packageName,
-            path: "./allshared/build/XCFrameworks/debug/\(packageName).xcframework"
+            url: remoteKotlinUrl,
+            checksum: remoteKotlinChecksum
         )
         ,
     ]
